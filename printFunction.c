5852baed2f0b69b37b9d52fb10640b643258abf4
Omar Ashraf <omarashraf100001@gmail.com>
2023-04-24 08:30:21 -0700

    fix betty

--- printFunction.c
+++ printFunction.c
@@ -1,24 +1,26 @@
 #include "main.h"
 /**
- * print_char - ....
- * @argumentToPrint: ...
- * @params: ...
- * Return: lenght
+ * print_char - print char
+ * @argumentToPrint: argument to print
+ * @param: unused parameter
+ * Return: length of output
  */
 int print_char(va_list argumentToPrint, __attribute__((unused)) param_t *param)
 {
 	char charTest;
+
 	charTest = va_arg(argumentToPrint, int);
 	putchar(charTest);
 	return (1);
 }
 /**
- * print_string - ...
- *  @argumentToPrint: ...
- * @params: ...
- * Return: lenght
+ * print_string - print string
+ * @argumentToPrint: argument to print
+ * @param: unused parameter
+ * Return: length of output
  */
-int print_string(va_list argumentToPrint, __attribute__((unused)) param_t *param)
+int print_string(va_list argumentToPrint,
+		__attribute__((unused)) param_t *param)
 {
 	char *stringTest;
 
@@ -27,12 +29,13 @@ int print_string(va_list argumentToPrint, __attribute__((unused)) param_t *param
 	return (strlen(stringTest));
 }
 /**
- * print_percentage - ...
- * @argumentToPrint: ...
- * @params: ...
- * Return: lenght
+ * print_percentage - print percentage
+ * @argumentToPrint: unused argument
+ * @param: unused parameter
+ * Return: length of output
  */
-int print_percentage(va_list __attribute__((unused)) argumentToPrint, __attribute__((unused)) param_t *param)
+int print_percentage(va_list __attribute__((unused)) argumentToPrint,
+			__attribute__((unused)) param_t *param)
 {
 	putchar('%');
 	return (1);
